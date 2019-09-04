41c51080a0b0f9f33c26e87ce3e49afe3baa5657
viniciusburza <vi-burza@hotmail.com>
2019-09-04 00:36:08 -0300

    Adicionado APP Lista de Tarefas

---
+++ lista_de_tarefas/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/views/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.teal)));
+}
